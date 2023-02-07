12020abb5b869e2aa56f968a7ee971a2163e43a9
Muno Ali <amunoali@gmail.com>
2023-02-07 08:02:43 -0500

    added User Route and Model

---
+++ api/models/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: { 
+        type: String, 
+        unique: true 
+    },
+    email: { 
+        type: String, 
+        unique: true 
+    },
+    password: String,
+});
+
+
+
+module.exports = mongoose.model("User", UserSchema);
