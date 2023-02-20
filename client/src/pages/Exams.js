a39661847fb08925f387d6b93e8e2f4f7368b87c
Parsua <patoofi@umass.edu>
2023-02-20 16:59:38 -0500

    Sua commit: added basic table html + css

--- client/src/pages/Exams.js
+++ client/src/pages/Exams.js
@@ -3,7 +3,40 @@ import 'react-bootstrap'
 
 function Exams(){
     return(
+        <>
         <h1>Hello From Exams</h1>
+
+        <div className='containerExams'>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Patient ID</th>
+                        <th>Exam ID</th>
+                        <th>Image</th>
+                        <th>Key Findings</th>
+                        <th>Brixia Score</th>
+                        <th>Age</th>
+                        <th>Sex</th>
+                        <th>BMI</th>
+                        <th>Zip Code</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>COVID-19-AR-16434381</td> 
+                        <td>Exam-1</td>
+                        <td>IMAGE LOL</td>
+                        <td>No relevant findings</td>
+                        <td>1,2,3,4</td>
+                        <td>44</td>
+                        <td>F</td>
+                        <td>64.6</td>
+                        <td>721</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        </>
     )
 }
 export default Exams;
