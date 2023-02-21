96428dee0002b78d33b0dd0c42f9bb4d502c1a1b
MiClaude07 <122179197+MiClaude07@users.noreply.github.com>
2023-02-21 10:36:27 -0500

    Added buttons to Exam Admin

--- client/src/pages/ExamAdmin.js
+++ client/src/pages/ExamAdmin.js
@@ -1,25 +1,32 @@
 import React from 'react'
 import 'react-bootstrap'
-
 import {useApi} from "../hooks/use-api";
 import "../css/Patient.css";
-import { Link, useParams } from "react-router-dom";
+import {Link} from "react-router-dom";
 import "../css/ExamAdmin.css"
+import { Button } from 'react-bootstrap';
 
 const AdminDisplay = (props) => {
+    const handleClick = () => {
+        console.log("Test Click");
+    };
     return (
         <>
-            <tr>
-                <td></td> 
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
+        <tr>
+            <td className = "examPageLink"><Link to ={`/patient/${props.patient.patientId}`}>{props.patient.patientId}</Link></td> 
+            <td className = "examPageLink"><Link to = {`/exam/${props.patient.examId}`}>{props.patient.examId}</Link></td>
+            <td><img src={props.patient.imageURL}/></td>
+            <td>{props.patient.keyFindings}</td>
+            <td>{props.patient.brixiaScores}</td>
+            <td>{props.patient.age}</td>
+            <td>{props.patient.sex}</td>
+            <td>{props.patient.bmi}</td>
+            <td>{props.patient.zipCode}</td>
+            <td className="adminButtons">
+                <button className="editButton" onClick={handleClick}>Edit</button>
+                <button className="removeButton" onClick={handleClick}>Remove</button>
+            </td>
+        </tr>
         </>
     );
 };
@@ -27,7 +34,6 @@ const AdminDisplay = (props) => {
 export const ExamAdmin = (props) => {
     const { response } = useApi({path : `patient` });
     let messages = [];
-    let ids = [];
   
     if (response) {
         messages = JSON.parse(response).message;
@@ -53,7 +59,7 @@ export const ExamAdmin = (props) => {
                     </tr>
                 </thead>
                 <tbody>
-                    {/* messages.map function here */}
+                    {messages.map((patient) => (<AdminDisplay patient={patient}/>))}
                 </tbody>
             </table>
         </div>
