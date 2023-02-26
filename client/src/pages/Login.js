bf2f4aa83c225a463740a679b64f98183eacca53
luissoldevilla <luis.angelsoto@hotmail.com>
2023-02-26 01:42:48 -0500

    Made the skeleton for the Login Page: made it the the mdb library

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,11 +1,51 @@
 import React from 'react'
-import Footer from '../components/Footer'
+import 'react-bootstrap';
+import {
+  MDBBtn,
+  MDBContainer,
+  MDBRow,
+  MDBCol,
+  MDBInput
+} from 'mdb-react-ui-kit';
+
 
 export default function Login() {
   return (
-    <div>
-        <h1>  Login Page </h1>
-        <Footer />
-    </div>
+    <>
+
+    <MDBContainer className="my-5 gradient-form">
+
+    <MDBRow>
+
+      <MDBCol col='6' className="mb-5">
+        <div className="d-flex flex-column ms-5">
+
+          <h1>Please login to your account</h1>
+
+
+          <MDBInput wrapperClass='mb-4' label='Email address' id='form1' type='email'/>
+          <MDBInput wrapperClass='mb-4' label='Password' id='form2' type='password'/>
+
+
+          <div className="text-center pt-1 mb-5 pb-1">
+            <MDBBtn className="mb-4 w-100">Sign in</MDBBtn>
+            <a className="mb-4 w-100" href="#!">Forgot password?</a>
+          </div>
+
+          <div className="d-flex flex-row align-items-center justify-content-center pb-4 mb-4">
+            <h4 className="mb-0">Don't have an account?</h4>
+            <MDBBtn outline className='mx-2' color='danger'>
+              Register
+            </MDBBtn>
+          </div>
+
+        </div>
+
+      </MDBCol>
+
+    </MDBRow>
+
+  </MDBContainer>
+    </>
   )
 }
