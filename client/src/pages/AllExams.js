dc777483d9f00a8309a385b61655a0c729744e21
MiClaude07 <122179197+MiClaude07@users.noreply.github.com>
2023-03-12 11:29:00 -0400

    Added sorting to All Exams

--- client/src/pages/AllExams.js
+++ client/src/pages/AllExams.js
@@ -7,11 +7,31 @@ import { useState, useEffect,  } from 'react';
 import "../css/Patient.css";
 import "../css/ExamPage.css"
 import Footer from '../components/Footer'
+import tableSort from '../components/SortedTable';
 const  API_URL = "https://coderyders-api.onrender.com"
+
+
 
 function AllExams(){
     const [exam, setExam] = useState([])
-
+    const [up_down, setUp_Down] = useState({
+      byPatientId: false, 
+      byExamId: false, 
+      byBrixia: false, 
+      byAge: false, 
+      byBMI: false, 
+      byZip: false});
+  const [list, setList] = useState([[{
+      patientId: "",
+      _id: "",
+      imageURL: "",
+      keyFindings: "",
+      brixiaScores: "",
+      age: "",
+      sex: "",
+      bmi: "",
+      zipCode: "",
+  }]]);
  
     
     useEffect(()=> {
@@ -28,6 +48,37 @@ function AllExams(){
                 console.log(err)
            })
       }
+    
+    const handleSort = (props) => {
+        let sort = tableSort(props, exam, up_down);
+        setList(sort);
+        switch(props) {
+            case "byPatientId":
+                setUp_Down({byPatientId: !up_down.byPatientId});
+                break;
+            case "byExamId":
+                setUp_Down({byExamId: !up_down.byExamId});
+                break;
+            case "byBrixia":
+                setUp_Down({byBrixia: !up_down.byBrixia});
+                break;
+            case "byAge":
+                setUp_Down({byAge: !up_down.byAge});
+                break;
+            case "byBMI":
+                setUp_Down({byBMI: !up_down.byBMI});
+                break;
+            case "byZip":
+                setUp_Down({byZip: !up_down.byZip});
+                break;
+           default:
+            return; 
+        }
+        console.log("After sort");
+        console.log(up_down);
+    };
+    let display = [];
+    display = (list.length < 2) ? [...exam] : [...list];
 
 
     return(
@@ -40,20 +91,43 @@ function AllExams(){
 <h1>Exams</h1>
     <table  className='container '>
         <thead>
-            <tr>
-                <th >Patient ID</th>
-                <th>Exam ID</th>
-                <th>Image</th>
-                <th>Key Findings</th>
-                <th>Brixia Score</th>
-                <th>Age</th>
-                <th>Sex</th>
-                <th>BMI</th>
-                <th>Zip Code</th>
-      
+            <tr className='headButt'>
+            <th>
+              <button className ="headButton" onClick={() => {handleSort("byPatientId")}}>
+                Patient ID
+              </button>
+            </th>
+            <th>
+              <button className="headButton" onClick={() => {handleSort("byExamId")}}>
+                Exam ID
+              </button>
+            </th>
+            <th>Image</th>
+            <th>Key Findings</th>
+            <th>
+              <button className="headButton" onClick={() => {handleSort("byBrixia")}}>
+                Brixia Scores
+              </button> 
+            </th>
+            <th>
+              <button className="headButton" onClick={() => {handleSort("byAge")}}>
+                Age
+              </button>
+            </th>
+            <th>Sex</th>
+            <th>
+              <button className="headButton" onClick={() => {handleSort("byBMI")}}>
+                BMI
+              </button>
+            </th>
+            <th>
+              <button className="headButton" onClick={() => {handleSort("byZip")}}>
+                Zip Code
+              </button>
+            </th>
             </tr>
         </thead>
-        {exam.map((exams) => (
+        {display.map((exams) => (
        
         <tbody>
           <tr>
